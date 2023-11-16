4a12824dba20716a4e5f16d1c4f085632758b1c7
IneSenaf8 <senaferreiroines@gmail.com>
2023-11-15 22:52:14 -0300

    cambios

--- js/script_mipeque.js
+++ js/script_mipeque.js
@@ -1,5 +1,6 @@
+
 let currentSlide = 0;
-const slides = document.querySelectorAll('.slide'); // hace que cambien de color el slider cuando pao el puntero //
+const slides = document.querySelectorAll('.slide');
 
 function showSlide(n) {
   slides[currentSlide].style.display = 'none';
@@ -13,12 +14,71 @@ function changeSlide(n) {
 
 showSlide(currentSlide);
 
-const wheel = document.querySelector('.wheel');
-const spinButton = document.querySelector('.spin-button');
 
+//tateti//
+const board = document.getElementById('board');
+    let currentPlayer = 'X';
+    const cells = Array.from({ length: 9 });
 
+// Función para crear las casillas del tablero
+    function createBoard() {
+        for (let i = 0; i < 9; i++) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            cell.dataset.index = i;
+            cell.addEventListener('click', handleCellClick);
+            board.appendChild(cell);
+            cells[i] = cell;
+        }
+    }
 
+// Función que maneja el clic en una casilla
+    function handleCellClick(e) {
+        const clickedCell = e.target;
+        const index = clickedCell.dataset.index;
 
+        if (cells[index].textContent === '') {
+            cells[index].textContent = currentPlayer;
+            checkWinner();
+            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        }
+    }
 
+ // Función para verificar si hay un ganador
+    function checkWinner() {
+        const winningCombos = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8], // Horizontales
+            [0, 3, 6], [1, 4, 7], [2, 5, 8], // Verticales
+            [0, 4, 8], [2, 4, 6] // Diagonales
+        ];
 
+        for (const combo of winningCombos) {
+            const [a, b, c] = combo;
+            if (
+                cells[a].textContent &&
+                cells[a].textContent === cells[b].textContent &&
+                cells[a].textContent === cells[c].textContent
+            ) {
+                alert(`¡Felicitaciones ${cells[a].textContent} ganaste un 20% de descuento!`);
+                resetBoard();
+                return;
+            }
+        }
+
+        if (cells.every(cell => cell.textContent !== '')) {
+            alert("¡Empate!");
+            resetBoard();
+        }
+    }
+
+    // Función para reiniciar el tablero
+    function resetBoard() {
+        cells.forEach(cell => {
+            cell.textContent = '';
+        });
+        currentPlayer = 'X';
+    }
+
+    // Inicialización del juego
+    createBoard();
 
